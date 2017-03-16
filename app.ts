367f3f72a1a6458f2a789b8a954939b89d559e20
Steven Myers <slmyers@ualberta.ca>
2017-03-15 20:15:52 -0600

    (progress) pchar changed to satisfy -- generalize the match condition to allow for more abstract parser, ie, parse character, parse binary, parse kitten pictures etc.

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ type Response = [ string, boolean, string, string ];
 type Character = string;
 const isCharacter = (data: any): data is Character => typeof data === 'string' && data.length === 1; 
 
-const pchar = (targetChar: Character, label: string): Parser => {
+const satisfy = (predicate: (thing: any) => boolean, label: string): Parser => {
 
     return (input$: Rx.Observable<Response>): Rx.Observable<Response> => {
         return input$
@@ -14,7 +14,7 @@ const pchar = (targetChar: Character, label: string): Parser => {
             .mergeMap(resp => {
                 const [ , , progress, str ] = resp
                 const [car, cdr] = [ str[0], str.substr(1, str.length - 1) ]
-                return Rx.Observable.if( () => car === targetChar, 
+                return Rx.Observable.if( () => predicate(car), 
                     Rx.Observable.of([label, true, progress + car , cdr]), 
                     Rx.Observable.throw([label, false, progress, str])
                 )
@@ -55,13 +55,13 @@ const orElse: ParserCombinator = (parser1, parser2, label) => {
 
 const anyOf = (chars: string) => {
     return chars.split('')
-                .map( char => pchar(char, `[pchar: ${char}]`))
+                .map( char => satisfy((car) => car === char, `[pchar: ${char}]`))
                 .reduce( (all, parser) => all === undefined ? parser : orElse(all, parser, `[anyOf: ${chars}]`));
 }
 
 const pstring = (chars: string) => {
     return chars.split('')
-                .map( char => pchar(char, `[pchar: ${char}]`))
+                .map( char => satisfy((car) => car === char, `[pchar: ${char}]`))
                 .reduce( (all, parser) => all === undefined ? parser : andThen(all, parser, `[pstring: ${chars}]`));
 }
 
@@ -79,7 +79,7 @@ const zeroOrMore = (parser1, chars) => {
 
 const many = (chars: string) => {
     const internalParsers =  chars.split('')
-                .map(char => pchar(char, `[pchar: ${char}]`))
+                .map( char => satisfy((car) => car === char, `[pchar: ${char}]`))
                 .reduce ( (all, parser) => all === undefined ? parser : orElse(all, parser, `[many: ${chars}]`) );
 
     return zeroOrMore(internalParsers, `many: ${chars}`);
@@ -88,28 +88,28 @@ const many = (chars: string) => {
 /* 
 
     const logFn = _ => console.log(_)
-    const pchar_a = pchar('a', `[pchar: a]`);
-    const pchar_b = pchar('b', `[pchar: b]`);
-    const pchar_c = pchar('c', `[pchar: c]`);
-    const pchar_e = pchar('e', `[pchar: e]`);
+    const pchar_a = satisfy( car => car === 'a', `[pchar: a]`);
+    const pchar_b = satisfy( car => car === 'b', `[pchar: b]`);
+    const pchar_c = satisfy( car => car === 'c', `[pchar: c]`);
+    const pchar_e = satisfy( car => car === 'e', `[pchar: e]`);
     usage: 
 
-    const parser = orElse(pchar('b', `[pchar: b]`), pchar('a', `[pchar: a]`), `[orElse: a, b]`);
-    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe( val => console.log(val));
+    const parser = orElse(pchar_b, pchar_a, `[orElse: a, b]`);
+    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe(logFn);
     [ '[orElse: a, b]', true, 'a', 'b' ]
 
     const parser = andThen(pchar('a', `[pchar: a]`), pchar('b', `[pchar: b]`), `[andThen: a, b]`);
-    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe( val => console.log(val));
+    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe(logFn);
     [ '[andThen: a, b]', true, 'ab', '' ]
 
     const a_or_b = orElse(pchar_a, pchar_b, `[orElse: a, b]`);
     const c_or_e = orElse(pchar_c, pchar_e, `[orElse: c, e]`);
     const a_or_b_andThen_c_or_e = andThen(a_or_b, c_or_e, `[andThen: a-or-else-b, c-or-else-e]`);
-    Rx.Observable.of(<Response>['', false, '', 'ae']).let(a_or_b_andThen_c_or_e).subscribe( val => console.log(val));
+    Rx.Observable.of(<Response>['', false, '', 'ae']).let(a_or_b_andThen_c_or_e).subscribe(logFn);
     [ '[andThen: a-or-else-b, c-or-else-e]', true, 'ae', '' ]
 
     const parser = anyOf('abc');
-    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe( val => console.log(val));
+    Rx.Observable.of(<Response>['', false, '', 'ab']).let(parser).subscribe(logFn);
     [ '[anyOf: abc]', true, 'a', 'b' ]
 
     const parser = many('1234567890');
@@ -120,6 +120,6 @@ const many = (chars: string) => {
     ' is my area code' ]
 
     const stringParser = pstring('blah');
-    Rx.Observable.of(<Response>['', false, '', 'blah']).let(stringParser).subscribe( val => console.log(val));
+    Rx.Observable.of(<Response>['', false, '', 'blah']).let(stringParser).subscribe(logFn);
     [ '[pstring: blah]', true, 'blah', '' ]
 */
